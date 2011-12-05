179a5bee74f5b9335a015af76aafa771b47dd2c3
Joao Lopes <johnny.online@gmail.com>
2011-12-05 01:55:11 +0000

    with profile edit

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -10,9 +10,43 @@
   end
 
   def menu
+    user = User.find_by_id(session[:user_id])
+    meetings = user.meetings
+    @past_meetings = []
+    @next_meetings = []
+    meetings.each do |meeting|
+      if meeting.datetime > Time.now
+        @next_meetings << meeting
+      else
+        @past_meetings << meeting
+      end
+    end
   end
 
   def login
+  end
+  
+  def edit
+    @user = User.find_by_id(session[:user_id])
+  end
+  
+  def update
+    @user = User.find_by_id(session[:user_id])
+    if @user.update_attributes(params[:user])
+      if @user.name
+        session[:username] = @user.name
+      else
+        session[:username] = @user.email
+      end
+      flash[:notice] = "Profile updated."
+      redirect_to(:action => 'menu')
+    else
+      flash[:notice] = ""
+      @user.errors.full_messages.each do |msg|
+        flash[:notice] << msg
+      end
+      render('edit')
+    end
   end
 
   def attempt_login
