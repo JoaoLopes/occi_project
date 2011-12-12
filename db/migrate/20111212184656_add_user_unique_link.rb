0f636220d5cbd1374d4ec05a63be85003529db95
Joao Lopes <johnny.online@gmail.com>
2011-12-12 21:04:27 +0000

    cenas

--- db/migrate/20111212184656_add_user_unique_link.rb
+++ db/migrate/20111212184656_add_user_unique_link.rb
@@ -1,7 +1,11 @@
 class AddUserUniqueLink < ActiveRecord::Migration
   def up
+    add_column("users", "permalink", :string)
+    add_column("meeting_people", "rsvp", :string, :default => "x")
   end
 
   def down
+    remove_column("meeting_people", "rsvp")
+    remove_column("users", "permalink")
   end
 end
